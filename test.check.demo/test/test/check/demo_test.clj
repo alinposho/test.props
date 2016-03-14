e52aeeb420aa31e07d06e01f026a63a4026fd51b
Alin Posorovaschi <alin.posorovaschi@e-spres-oh.com>
2016-03-14 12:57:26 +0200

    Add an example of using the seed to debug tests

--- test.check.demo/test/test/check/demo_test.clj
+++ test.check.demo/test/test/check/demo_test.clj
@@ -5,16 +5,16 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer :all]))
 
-(defspec first-element-is-min-after-sorting ;; the name of the test
-         100 ;; the number of iterations for test.check to test
+(defspec first-element-is-min-after-sorting                 ;; the name of the test
+         100                                                ;; the number of iterations for test.check to test
          (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
-           (= (apply min v)
-              (first (sort v)))))
+                       (= (apply min v)
+                          (first (sort v)))))
 
 (defspec sort-is-idempotent
-  100
-  (prop/for-all [v (gen/vector gen/int)]
-    (= (sort v) (sort (sort v)))))
+         100
+         (prop/for-all [v (gen/vector gen/int)]
+                       (= (sort v) (sort (sort v)))))
 
 (comment
   (run-tests *ns*)
