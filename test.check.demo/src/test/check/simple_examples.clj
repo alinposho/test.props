f38157402824c961cc5d73bdddf61ccf7734b478
Alin Posorovaschi <alin.posorovaschi@e-spres-oh.com>
2016-03-24 10:42:08 +0200

    Reorganize code for presentation

--- test.check.demo/src/test/check/simple_examples.clj
+++ test.check.demo/src/test/check/simple_examples.clj
@@ -4,11 +4,22 @@
             [clojure.test.check.properties :as prop]))
 
 
+(def min-is-smaller-than-all-elements
+  (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
+                (let [min-elem (apply min v)]
+                  (every? (fn [elem] (<= min-elem elem)) v))))
+
+(comment
+  (tc/quick-check 1000 min-is-smaller-than-all-elements)
+  )
+
+
 (defn ascending?
   "clojure.core/sorted? doesn't do what we might expect, so we write our
   own function"
   [coll]
   (every? (fn [[a b]] (<= a b)) (partition 2 1 coll)))
+
 
 (def ascending-sorted-vector-prop
   (prop/for-all [v (gen/vector gen/int)]
@@ -20,15 +31,9 @@
                 (let [s (sort > v)]
                   (not (ascending? s)))))
 
-(def min-is-smaller-than-all-elements
-  (prop/for-all [v (gen/not-empty (gen/vector gen/int))]
-                (let [min-elem (apply min v)]
-                  (every? (fn [elem] (<= min-elem elem)) v))))
-
 (comment
   (tc/quick-check 100 ascending-sorted-vector-prop)
   (tc/quick-check 100 descending-sorted-vector-prop)
   (tc/quick-check 100 min-is-smaller-than-all-elements :seed 1457950593723)
-  (tc/quick-check 1000 min-is-smaller-than-all-elements)
 
   )
